3962cf2878b2da3b278874c7f753dd67362dfb61
Kaedras <6783810+Kaedras@users.noreply.github.com>
2024-07-20 15:38:46 +0200

    Changed type of paths from wstring to filesystem::path Changed other wstring uses to native_string (wstring on windows, string on linux) Made DLLEXPORT macro linux compatible

--- include/archive/archive.h
+++ include/archive/archive.h
@@ -22,16 +22,27 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #define ARCHIVE_H
 
 #include <cstdint>
+#include <filesystem>
 #include <functional>
 #include <memory>
 #include <string>
 
+#ifdef _WIN32
+using native_string = std::wstring;
+#define EXPORT __declspec(dllexport)
+#define IMPORT __declspec(dllimport)
+#else
+using native_string = std::string;
+#define EXPORT __attribute__((visibility("default")))
+#define IMPORT __attribute__((visibility("hidden")))
+#endif
+
 #if defined(MO2_ARCHIVE_BUILD_STATIC)
 #define DLLEXPORT
 #elif defined(MO2_ARCHIVE_BUILD_EXPORT)
-#define DLLEXPORT _declspec(dllexport)
+#define DLLEXPORT EXPORT
 #else
-#define DLLEXPORT _declspec(dllimport)
+#define DLLEXPORT IMPORT
 #endif
 
 class FileData
@@ -41,7 +52,7 @@ public:
    * @return the path of this entry in the archive (usually relative, unless the archive
    *   contains absolute path).
    */
-  virtual std::wstring getArchiveFilePath() const = 0;
+  virtual std::filesystem::path getArchiveFilePath() const = 0;
 
   /**
    * @return the size of this entry in bytes (uncompressed).
@@ -54,7 +65,7 @@ public:
    *
    * @param filepath The filepath to add, relative to the output folder.
    */
-  virtual void addOutputFilePath(std::wstring const& filepath) = 0;
+  virtual void addOutputFilePath(std::filesystem::path const& filepath) = 0;
 
   /**
    * @brief Retrieve the list of filepaths to extract this entry to.
@@ -62,7 +73,7 @@ public:
    * @return the list of paths this entry should be extracted to, relative to the
    *   output folder.
    */
-  virtual const std::vector<std::wstring>& getOutputFilePaths() const = 0;
+  virtual const std::vector<std::filesystem::path>& getOutputFilePaths() const = 0;
 
   /**
    * @brief Clear the list of output file paths for this entry.
@@ -100,7 +111,7 @@ public:  // Declarations
     // extracting
     // a lot of files, this may reach 100% way before the extraction is complete since
     // most of the
-    // time will be spend writing data and not reading it (use EXTRACTION in this case).
+    // time will be spent writing data and not reading it (use EXTRACTION in this case).
     // When
     // extracting few small files,  this may be useful for solid archives since most of
     // the time
@@ -129,11 +140,11 @@ public:  // Declarations
   /**
    * List of callbacks:
    */
-  using LogCallback        = std::function<void(LogLevel, std::wstring const& log)>;
+  using LogCallback        = std::function<void(LogLevel, native_string const& log)>;
   using ProgressCallback   = std::function<void(ProgressType, uint64_t, uint64_t)>;
-  using PasswordCallback   = std::function<std::wstring()>;
-  using FileChangeCallback = std::function<void(FileChangeType, std::wstring const&)>;
-  using ErrorCallback      = std::function<void(std::wstring const&)>;
+  using PasswordCallback   = std::function<native_string()>;
+  using FileChangeCallback = std::function<void(FileChangeType, std::filesystem::path const&)>;
+  using ErrorCallback      = std::function<void(native_string const&)>;
 
   /**
    *
@@ -192,7 +203,7 @@ public:
    *
    * @return true if the archive was open properly, false otherwise.
    */
-  virtual bool open(std::wstring const& archivePath,
+  virtual bool open(std::filesystem::path const& archivePath,
                     PasswordCallback passwordCallback) = 0;
 
   /**
@@ -221,7 +232,7 @@ public:
    *
    * @return true if the archive was extracted, false otherwise.
    */
-  virtual bool extract(std::wstring const& outputDirectory,
+  virtual bool extract(std::filesystem::path const& outputDirectory,
                        ProgressCallback progressCallback,
                        FileChangeCallback fileChangeCallback,
                        ErrorCallback errorCallback) = 0;
@@ -232,30 +243,30 @@ public:
   virtual void cancel() = 0;
 
   // A bunch of useful overloads (with one or two callbacks):
-  bool extract(std::wstring const& outputDirectory, ErrorCallback errorCallback)
+  bool extract(std::filesystem::path const& outputDirectory, ErrorCallback errorCallback)
   {
     return extract(outputDirectory, {}, {}, errorCallback);
   }
-  bool extract(std::wstring const& outputDirectory, ProgressCallback progressCallback)
+  bool extract(std::filesystem::path const& outputDirectory, ProgressCallback progressCallback)
   {
     return extract(outputDirectory, progressCallback, {}, {});
   }
-  bool extract(std::wstring const& outputDirectory,
+  bool extract(std::filesystem::path const& outputDirectory,
                FileChangeCallback fileChangeCallback)
   {
     return extract(outputDirectory, {}, fileChangeCallback, {});
   }
-  bool extract(std::wstring const& outputDirectory, ProgressCallback progressCallback,
+  bool extract(std::filesystem::path const& outputDirectory, ProgressCallback progressCallback,
                ErrorCallback errorCallback)
   {
     return extract(outputDirectory, progressCallback, {}, errorCallback);
   }
-  bool extract(std::wstring const& outputDirectory, ProgressCallback progressCallback,
+  bool extract(std::filesystem::path const& outputDirectory, ProgressCallback progressCallback,
                FileChangeCallback fileChangeCallback)
   {
     return extract(outputDirectory, progressCallback, fileChangeCallback, {});
   }
-  bool extract(std::wstring const& outputDirectory,
+  bool extract(std::filesystem::path const& outputDirectory,
                FileChangeCallback fileChangeCallback, ErrorCallback errorCallback)
   {
     return extract(outputDirectory, {}, fileChangeCallback, errorCallback);
