17c11f83596effa1933452506ba240ad69b44681
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-05-21 00:49:55 +0200

    Add `NoOutputPaths` test, create macro for test initialization

--- tests/test.cpp
+++ tests/test.cpp
@@ -21,6 +21,16 @@ void errorCallback(const QString& log)
   std::cerr << log.toStdString() << '\n';
 }
 
+// create tmp dir and open an archive
+#define INIT(filename)                                                                 \
+  QTemporaryDir tmpDir;                                                                \
+  ASSERT_TRUE(tmpDir.isValid()) << tmpDir.errorString().toStdString();                 \
+  auto a = CreateArchive();                                                            \
+  ASSERT_TRUE(a->isValid()) << a->errorString().toStdString();                         \
+  a->setLogCallback(logCallback);                                                      \
+  ASSERT_TRUE(a->open("files/"s + filename, passwordCallback))                         \
+      << a->errorString().toStdString()
+
 class ArchiveTest : public testing::TestWithParam<string>
 {
   // You can implement all the usual fixture class members here.
@@ -32,15 +42,7 @@ TEST_P(ArchiveTest, Archive)
 {
   const string& archive = GetParam();
 
-  QTemporaryDir tmpDir;
-  ASSERT_TRUE(tmpDir.isValid()) << tmpDir.errorString().toStdString();
-
-  auto a = CreateArchive();
-  ASSERT_TRUE(a->isValid()) << a->errorString().toStdString();
-  a->setLogCallback(logCallback);
-
-  ASSERT_TRUE(a->open("files/" + archive, passwordCallback))
-      << a->errorString().toStdString();
+  INIT(archive);
 
   vector<FileData*> files = a->getFileList();
   for (FileData* file : files) {
@@ -56,6 +58,20 @@ INSTANTIATE_TEST_SUITE_P(Extract, ArchiveTest,
                          testing::Values("test.7z", "test_encrypted.7z",
                                          "test_encrypted_headers.7z", "test.zip",
                                          "test_encrypted.zip"));
+
+TEST(ArchiveTest, NoOutputPaths)
+{
+  INIT("test.7z");
+
+  ASSERT_TRUE(a->extract(tmpDir.path().toStdString(), nullptr, nullptr, errorCallback))
+      << a->errorString().toStdString();
+
+  QDir dir(tmpDir.path());
+
+  auto entryList = dir.entryList(QDir::AllEntries | QDir::NoDotAndDotDot);
+  ASSERT_TRUE(entryList.isEmpty())
+      << "Directory contains " << entryList.join(';').toStdString();
+}
 
 // INSTANTIATE_TEST_SUITE_P(ExtractNested, ArchiveTest,
 //                          testing::Values("test.tar.bz2",
